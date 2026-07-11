8b5cacfbeeea902c64aaa3114f5757858c023bd9
atiqihazizan <aqi852000@gmail.com>
2026-07-11 10:44:54 +0800

    buang node-react-jssor-page-designer (backup di v0.2.0)

--- node-react-jssor-page-designer/react/src/processors/slideHelpers.js
+++
@@ -1,7 +0,0 @@
-/**
- * Helper bersama untuk slide processors (escape HTML).
- * Logic batal/skip/replace kini di backend; frontend hanya papar data diproses.
- */
-
-export const escapeHtml = (s) =>
-  String(s || '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
